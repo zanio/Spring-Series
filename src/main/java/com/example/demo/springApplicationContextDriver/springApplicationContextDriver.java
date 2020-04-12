0d78c10a00219c8e79061fd78b070846f6df9cff
ANIEFIOK AKPAN <akp.ani@yahoo.com>
2020-04-12 19:11:36 +0100

    chore(logger):instantiated logger object

--- src/main/java/com/example/demo/springApplicationContextDriver/springApplicationContextDriver.java
+++ src/main/java/com/example/demo/springApplicationContextDriver/springApplicationContextDriver.java
@@ -1,19 +1,23 @@
 package com.example.demo.springApplicationContextDriver;
 
 import com.example.demo.springApplicationContext.XmlPersonDAO;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+
 @Configuration
 @ComponentScan("com.example.demo.springApplicationContext")
 public class springApplicationContextDriver {
+    private static final Logger logger = LoggerFactory.getLogger(springApplicationContextDriver.class);
     public static void main(String[] args) {
         try (ClassPathXmlApplicationContext classPathXmlApplicationContext =
                      new ClassPathXmlApplicationContext("applicationContext.xml")) {
             XmlPersonDAO xmlPersonDAO = classPathXmlApplicationContext.getBean(XmlPersonDAO.class);
-            System.out.println(xmlPersonDAO);
-            System.out.println(xmlPersonDAO.getXmlJdbcConnection());
+            logger.info("{}",xmlPersonDAO);
+            logger.info("{}",xmlPersonDAO.getXmlJdbcConnection());
         }
 
     }
